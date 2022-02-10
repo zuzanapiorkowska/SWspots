68f85519e5c8a09653444265084cefc42ca9b237
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-10 15:06:21 +0100

    fix classes

--- SWSpot.ts
+++ SWSpot.ts
@@ -1,25 +1,30 @@
+export class Location {
+    constructor(
+        public lat: number,
+        public lng: number
+    ) { }
+}
+
+export class Equipment {
+    constructor(
+        public bar: string,
+        public wideBar: string,
+        public highBar: string,
+        public parallelBars: string,
+        public ladder: string,
+        public horizontalLadder: string,
+        public pipe: string
+    ) { }
+}
 export class SWSpot {
     constructor(
-    placeName: string,
-    location:
-        {
-            lat: number,
-            lng: number
-        },
-    surface: string,
-    equipment:
-        {
-            bar: string,
-            wideBar: string,
-            highBar: string,
-            parallelBars: string,
-            ladder: string,
-            horizontalLadder: string,
-            pipe: string
-        },
-    img: string
-){}
+        public placeName: string,
+        public location: Location,
+        public surface: string,
+        public equipment: Equipment,
+        public img: string
+    ) { }
     markerContent(): string {
-        return `<h1>${this. placeName}</h1>`;
+        return `<h1>${this.placeName}</h1>`;
     }
 }
